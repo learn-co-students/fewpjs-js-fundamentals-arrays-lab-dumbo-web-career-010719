be09aaef7fe734ca436cc82e8cfbab1a2946e920
sbrij001 <sbrij001@yahoo.com>
2019-09-27 13:56:27 -0400

    slice, spread, splice

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function destructivelyRemoveLastDriver(){
 function destructivelyRemoveFirstDriver(){
   drivers.shift()
 }
+
+function appendDriver(name){
+  return [...drivers, name]
+};
+
+function prependDriver(name){
+  return [name,...drivers]
+}
+
+function removeLastDriver(){
+  return drivers.slice(0,-1)
+}
+
+function removeFirstDriver(){
+  return drivers.slice(1)
+}
