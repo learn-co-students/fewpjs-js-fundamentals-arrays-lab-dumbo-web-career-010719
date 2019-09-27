0e3e51e83b4f73dad5b9750cd47e55e856941693
sbrij001 <sbrij001@yahoo.com>
2019-09-27 13:29:12 -0400

    simple array manipulation methods

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-// Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"];
+const drivers =  ["Milo", "Otis", "Garfield"]
 
-
-function destructivelyAppendDriver(name) {
+function destructivelyAppendDriver(name){
   drivers.push(name)
-};
+}
 
-function destructivelyPrependDriver(name) {
+function destructivelyPrependDriver(name){
   drivers.unshift(name)
-};
+}
 
-function destructivelyRemoveLastDriver(name) {
-  drivers.pop(name)
-};
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}
 
-function destructivelyRemoveFirstDriver(name) {
-  drivers.shift(name)
-};
-
-function appendDriver(name) {
-  let newDrivers = [...drivers, name]
-  return newDrivers
-};
-
-function prependDriver(name) {
-  let newDrivers = [name,...drivers]
-  return newDrivers
-};
-
-function removeLastDriver() {
-  let newArray = drivers.slice(0,-1)
-  return newArray
-};
-
-function removeFirstDriver(){
-  let newArray = drivers.slice(1)
-  return newArray
-};
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+}
